b65148cc9555c9a99134af164c581140a1aa69a5
Magnus Ihse Bursie <mag@icus.se>
2026-09-17 22:04:18 +0200

    chore: Update modid to battery-level, and introduce standardized mod development setup

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 1.2+1.21 - 2026-09-17
+
+### Added
+
+- Created project by fork from [Battery Status Info](https://github.com/edge217/BatteryStatusInfo)
--- src/main/java/com/edgeburnmedia/batterystatusinfo/BatteryStatusInfoMod.java
+++ src/main/java/com/edgeburnmedia/batterystatusinfo/BatteryStatusInfoMod.java
@@ -3,7 +3,7 @@ package com.edgeburnmedia.batterystatusinfo;
 import net.fabricmc.api.ModInitializer;
 
 public class BatteryStatusInfoMod implements ModInitializer {
-	public static final String MOD_ID = "batterystatusinfo";
+	public static final String MOD_ID = "battery-level";
 
 	@Override
 	public void onInitialize() {
